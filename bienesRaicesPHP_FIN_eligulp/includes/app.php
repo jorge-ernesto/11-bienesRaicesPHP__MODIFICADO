88ce1d84e3d2f19e7eb51fa454810c1554faf925
Ernesto <jorge.cywdt@gmail.com>
2023-04-19 20:09:03 -0500

    Init commit

---
+++ bienesRaicesPHP_FIN_eligulp/includes/app.php
@@ -0,0 +1,11 @@
+<?php 
+
+define('TEMPLATES_URL', __DIR__ . '/templates');
+define('FUNCIONES_URL', __DIR__ . 'funciones.php');
+
+// echo "<pre>";
+// print_r(array(
+//     'TEMPLATES_URL' => TEMPLATES_URL,
+//     'FUNCIONES_URL' => FUNCIONES_URL,
+// ));
+// echo "</pre>";
---
+++ bienesRaicesPHP_FIN_eligulp/usuario.php
@@ -0,0 +1,20 @@
+<?php
+
+// Importar la conexión
+require 'includes/config/database.php';
+$db = conectarDB();
+
+// Crear un email y password
+$email = "admin@admin.com";
+$password = "admin";
+
+$passwordHash = password_hash($password, PASSWORD_BCRYPT);
+
+
+// Query para crear el usuario
+$query = " INSERT INTO usuarios (email, password) VALUES ( '${email}', '${passwordHash}'); ";
+
+echo $query;
+
+// Agregarlo a la base de datos
+mysqli_query($db, $query);
